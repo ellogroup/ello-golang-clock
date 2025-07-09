4a847c011abfd11308738b82f4c785b8a6485a80
Dave Richards <dave.richards@ellogroup.co.uk>
2025-07-09 09:02:17 +0100

    chore: renames constructors

--- clock/clock_test.go
+++ clock/clock_test.go
@@ -49,7 +49,7 @@ func TestFixed(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Equalf(t, tt.want, Fixed(tt.args.t), "Fixed(%v)", tt.args.t)
+			assert.Equalf(t, tt.want, NewFixed(tt.args.t), "NewFixed(%v)", tt.args.t)
 		})
 	}
 }
